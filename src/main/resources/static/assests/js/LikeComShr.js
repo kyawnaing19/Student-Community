73ae67cc65cec6f1225dd9a778bb92f626c0eace
amk-35 <aungmyokyaw@ucstt.edu.mm>
2024-07-31 17:13:08 +0630

    like count and comment count finished

--- src/main/resources/static/assests/js/LikeComShr.js
+++ src/main/resources/static/assests/js/LikeComShr.js
@@ -2,6 +2,13 @@ function addLike(userId,postId){
     $.get('/likes/addLike', { userId: userId, postId:postId }, function(response) {
 
          $('#LikeIcon-'+postId+'').attr('class','fa-brands fa-gratipay');
+        let currentValue = parseInt($('#likeCount').text(), 10);
+
+// Increment the value by 1
+        let newValue = currentValue + 1;
+
+// Set the updated value back to the span
+        $('#likeCount').text(newValue);
         $('#Like-'+postId+'').attr('onclick','deleteLike('+userId+','+postId+')');
 
     }).fail(function(xhr, status, error) {
@@ -16,6 +23,15 @@ function deleteLike(userId, postId) {
         data: { userId: userId, postId: postId },
         success: function(response) {
             $('#LikeIcon-'+postId+'').attr('class','fa fa-heart');
+            // Get the current value from the span, which is a string, and convert it to a number
+            let currentValue = parseInt($('#likeCount').text(), 10);
+
+// Decrement the value by 1
+            let newValue = currentValue - 1;
+
+// Set the updated value back to the span
+            $('#likeCount').text(newValue);
+
             $('#Like-'+postId+'').attr('onclick','addLike('+userId+','+postId+')');
         },
         error: function(xhr, status, error) {
